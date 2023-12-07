257d9317076b93a1d2a11ace08a5d1a1dd3e6fd0
LesostepnoyGnom <73924501+LesostepnoyGnom@users.noreply.github.com>
2023-12-07 23:11:58 +0300

    lab1

---
+++ lab1/lab1_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <omp.h>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+void test(const int len) {
+    const int len1 = pow(10, len);
+    cout << "f " << len1 << endl;
+    vector<int> v(len1);
+    random_device r;
+    generate(v.begin(), v.end(), [&] {return r(); });
+
+    for (int j = 0; j < 10; j++) {
+        int max = v[0];
+        auto start = chrono::high_resolution_clock::now();
+        #pragma omp parallel shared(v) num_threads(j)
+        for (int i = 0; i < len1; i++) {
+            if (v[i] > max) {
+                max = v[i];
+            }
+            //cout << "Thread(" << omp_get_thread_num() << ")" << "v_out = " << v_out[i] << endl;
+        }
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        cout << "num_threads = " << j + 1 << ", time = " << duration.count() << endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    const int len = atoi(argv[1]);
+    test(len);
+    return 1337;
+}
---
+++ lab1/lab1_2.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <omp.h>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+void test(const int size) {
+    cout << "matrix size = " << size << endl;
+
+    srand(time(NULL)); // Инициализируем генератор случайных чисел
+    int** a = new int* [size]; // Создаем массив указателей
+
+    for (int i = 0; i < size; i++) {
+        a[i] = new int[size]; // Создаем элементы
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            a[i][j] = rand() % 10; // Каждый элемент случайному числу от 0 до 9
+            //cout << a[i][j] << " "; // Вывести элементы на консольку
+        }
+        //cout << endl; // Двумерный массив. Строка кончилась, переводим строку и на консоли
+    }
+ 
+    int** b = new int* [size];
+    for (int i = 0; i < size; i++) {
+        b[i] = new int[size];
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            b[i][j] = rand() % 10;
+            //cout << b[i][j] << " ";
+        }
+        //cout << endl;
+    }
+
+    int** c = new int* [size];
+
+    for (int i = 0; i < size; i++) {
+        c[i] = new int[size];
+    }
+
+    /*cout << "matrix a:" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;   
+    }*/
+
+    /*cout << "matrix b:" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }*/
+
+    auto start1 = chrono::high_resolution_clock::now();
+
+        #pragma omp parallel num_threads(1)
+    {
+        #pragma omp for
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k < size; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+    }
+    auto stop1 = chrono::high_resolution_clock::now();
+    double t1;
+    auto dur1 = chrono::duration_cast<chrono::milliseconds>(stop1 - start1);
+    t1 = dur1.count();
+    //cout << "t1 = " << t1 << endl;
+    for (int nn = 1; nn < 11; nn++) {
+
+        auto start = chrono::high_resolution_clock::now();
+
+        #pragma omp parallel num_threads(nn+1)
+        {
+            #pragma omp for
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    c[i][j] = 0;
+                    for (int k = 0; k < size; k++) {
+                        c[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+            }
+        }
+        /*cout << "matrix c:" << endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                cout << c[i][j] << " ";
+            }
+            cout << endl;
+        }*/
+
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+        cout << "num threads = " << nn << ", t1/t = " << t1 / duration.count() << endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int n = atoi(argv[1]);
+    const int size = n * 1000;
+    test(size);
+    return 1337;
+}
